ecc2e1cfa80f023c4b78303888fb12a3d56f0a19
Hemish123 <consultant@jmsadvisory.in>
2026-08-07 11:35:50 +0530

    Client ID

--- config/settings.py
+++ config/settings.py
@@ -289,5 +289,5 @@ if USE_AZURE_MEDIA:
 else:
     MEDIA_URL  = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-GOOGLE_OAUTH_CLIENT_ID = config('GOOGLE_OAUTH_CLIENT_ID', default='498002626985-gu6fqr9n3ue8dkfvvkr7qfk91s2nb25n.apps.googleusercontent.com')
+GOOGLE_OAUTH_CLIENT_ID = config('GOOGLE_OAUTH_CLIENT_ID', default='543573978646-hgopbbdj7gvl3uvuatf5no529shg1q8r.apps.googleusercontent.com')
 GOOGLE_OAUTH_CLIENT_SECRET = config('GOOGLE_OAUTH_CLIENT_SECRET', default='')
